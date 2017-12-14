cde59353584bbb27c4ab810242d56fb67cd206c9
IBenCubing <i.ben.cubing@gmail.com>
2017-12-14 00:26:49 -0600

    more webpack

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,27 @@
+import './style.css'
+
+const head = document.head
+const body = document.body
+
+const style = (element: HTMLElement, style: string) => {
+	element.setAttribute('style', style)
+}
+
+const append = (target: HTMLElement, element: HTMLElement) => {
+	target.appendChild(element)
+}
+
 const init = () => {
-	document.body.setAttribute('style', 'padding: 0; margin: 0')
+	const title = document.createElement('title')
+	title.innerHTML = 'Symply'
+	append(head, title)
+}
+
+const component = () => {
+	const element = document.createElement('div')
+	element.innerHTML = 'test'
+	return element
 }
 
 init()
-
-document.write('Hello webpack!')
+append(body, component())
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,21 @@
 const path = require('path');
+const UglifyJS = require('uglifyjs-webpack-plugin')
 
 module.exports = {
 	entry: './src/index.ts',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist')
+	},
 	module: {
 		rules: [
+			{
+				test: /\.css$/,
+				use: [
+					'style-loader',
+					'css-loader'
+				]
+			},
 			{
 				test: /\.tsx?$/,
 				use: 'ts-loader',
@@ -11,11 +23,7 @@ module.exports = {
 			}
 		]
 	},
-	resolve: {
-		extensions: ['.tsx', '.ts', '.js']
-	},
-	output: {
-		filename: 'bundle.js',
-		path: path.resolve(__dirname, 'dist')
-	}
+	plugins: [
+		new UglifyJS()
+	]
 };
